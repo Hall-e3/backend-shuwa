d0d6b7988ac0d5a96aa3903fd02a1f4755b6b6da
Hall Enoch Asanda <hallenochasanda@gmail.com>
2023-02-27 11:27:20 +0300

    config

--- config/db.js
+++ config/db.js
@@ -2,9 +2,12 @@ import mongoose from "mongoose";
 
 const InitiateMongoServer = async () => {
   try {
-    await mongoose.connect(process.env.MONGO_URL, {
-      useNewUrlParser: true,
-    });
+    await mongoose.connect(
+      `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER_PASSWORD}@cluster0.divntla.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`,
+      {
+        useNewUrlParser: true,
+      }
+    );
     console.log("Connected to DB!!");
   } catch (e) {
     console.log(e);
